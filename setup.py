3660b2bc4c9cfebe79ebc492ed9082a8c3b7b3d4
Aurora Driveklepp Helgeland <aurora.helgeland@uit.no>
2023-12-12 11:39:35 -0500

    create setup file

---
+++ setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+name = "TDE-functions"
+
+with open("README.md") as f:
+    long_description = f.read()
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    name=name,
+    description="Python scripts for plotting velocity estimation.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/auroradh/TDE-functions",
+    license="MiT",
+    version="1.0",
+    packages=["cmod_functions"],
+    python_requires=">=3.0",
+    install_requires=[
+        "numpy>=1.18.0",
+        "scipy>=1.4.0",
+        "matplotlib>=3.2.0",
+        "xarray>=0.16.2",
+        "dask",
+        "netCDF4",
+        "bottleneck",
+    ],
+    classifiers=[
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Visualization",
+    ],
+    zip_safe=False,
+)
